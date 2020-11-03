af2eea8d7b2cd072a5305ecaa57b0902deb46c5f
SynCap <syncap@ya.ru>
2020-11-03 19:52:18 +0800

    code lint

--- EvdEnv.psm1
+++ EvdEnv.psm1
@@ -1,9 +1,10 @@
 
 # Управление сессионной переменной окружения PATH
 function .pc {$env:Path.Split(';')[-3..-1]}
-function .pp {if($env:Path -NotLike "*;$(pwd)"){$env:Path+=";$(pwd)"};.pc}
+function .pp {if($env:Path -NotLike "*;$pwd"){$env:Path+=";$pwd"};.pc}
 function .pd {$env:Path=$env:Path.Split(';')[0..-2].Join(';');.pc}
 
+# PowerShell:PSAvoidGlobalVars=$False
 $Global:EvdSPF = @{}
 function .spf ($SpecialFolderAlias) {
     # $keys = [Enum]::GetNames([System.Environment+SpecialFolder])
@@ -52,11 +53,13 @@ filter TotalCmd {
 
 # Set-Alias subl -Value "C:\Program Files\Sublime Text 3\subl.exe"
 
-$subl = Join-Path $env:ProgramFiles "Sublime Text 3" "subl.exe"
+$sublPath = Join-Path -Path $env:ProgramFiles -ChildPath "Sublime Text 3" "subl.exe"
 function subl {
     param (
-        [Parameter(Mandatory,ValueFromPipeline)] [String[]] $Path = '.'
+        [Parameter(ValueFromPipeline)] [String[]] $Path = '.'
     )
-    echo $args
-    & $subl ($Path, $args)
+    Process {
+        Write-Debug "`$args: $args"
+        & $sublPath ($Path, $args)
+    }
 }

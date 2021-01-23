3cdd439584788d424bd2038292190eaf6b26428c
Constantin Losk <SynCap@users.noreply.github.com>
2021-01-23 20:14:31 +0800

    upd(dlwp): new code

--- mod/Net.psm1
+++ mod/Net.psm1
@@ -2,10 +2,11 @@ function dlwp {
 	param(
 		[parameter(mandatory,position=0)] [String] $Url,
 		[Switch] $Bing,
-		[parameter(position=1)] [String] $Dest = $Bing ? 'Bing' : 'Yandex'
+		[parameter(position=1)] [String] $Dest = ( Join-Path 'D:\Graphics\WP\' ($Bing ? 'Bing' : 'Yandex') )
 	)
-	$Dest = join-path 'D:\Graphics\WP\' $Dest
-	$FName = (join-path $Dest (split-path $Url -leaf))
-	wget -O $FName $Url
+	$r = Invoke-WebRequest $Url
+	$Url -Match '(?<=/)[^/#?]+?(?<Ext>\.[^/.#?]*?)?(?=[#?]|$)';
+	$FName = Join-Path -Path $Dest -ChildPath ($Matches[0],($Matches.Ext ?? ('.' + ($r.Headers['Content-Type'] -split '/')[1] ?? '')) -join '')
+	Set-Content -AsByteStream -Value $r.Content -Path $FName
 	explorer.exe /select,$FName
 }

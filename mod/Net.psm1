2875eea8d94e53b7dd98a5d10452014f1981431d
SynCap <syncap@ya.ru>
2021-02-26 11:53:46 +0800

    upd(dlwp): +switches; +if match

--- mod/Net.psm1
+++ mod/Net.psm1
@@ -2,11 +2,14 @@ function dlwp {
 	param(
 		[parameter(mandatory,position=0)] [String] $Url,
 		[Switch] $Bing,
-		[parameter(position=1)] [String] $Dest = ( Join-Path 'D:\Graphics\WP\' ($Bing ? 'Bing' : 'Yandex') )
+		[parameter(position=1)] [String] $Dest = ( Join-Path -Path 'D:\Graphics\WP\' -ChildPath ($Bing ? 'Bing' : 'Yandex') )
 	)
 	$r = Invoke-WebRequest $Url
-	$Url -Match '(?<=/)[^/#?]+?(?<Ext>\.[^/.#?]*?)?(?=[#?]|$)';
-	$FName = Join-Path -Path $Dest -ChildPath ($Matches[0],($Matches.Ext ?? ('.' + ($r.Headers['Content-Type'] -split '/')[1] ?? '')) -join '')
-	Set-Content -AsByteStream -Value $r.Content -Path $FName
-	explorer.exe /select,$FName
+	if ($Url -Match '(?<=/)[^/#?]+?(?<Ext>\.[^/.#?]*?)?(?=[#?]|$)') {
+		$FName = Join-Path -Path $Dest -ChildPath ($Matches[0],($Matches.Ext ?? ('.' + ($r.Headers['Content-Type'] -split '/')[1] ?? '')) -join '')
+		Set-Content -AsByteStream -Value $r.Content -Path $FName
+		explorer.exe /select,$FName
+	} else {
+		Write-Error 'Bad URL'
+	}
 }

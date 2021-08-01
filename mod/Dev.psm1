61d39d85f71e6a9a4e405f07e685364fddaa6f03
SynCap <syncap@ya.ru>
2021-08-02 00:10:11 +0800

    chore: code compacting

--- mod/Dev.psm1
+++ mod/Dev.psm1
@@ -101,7 +101,8 @@ function start {
 # загрузить в Sublime тему от o-my-posh
 function Edit-Theme ($name) {
 	Get-Theme | Where-Object Name -ilike $name | ForEach-Object {
-		Write-Verbose "Open theme `"{0}`" in SublimeText`n{1}" -f $_.Name,$_.Location
+		Write-Verbose "Open theme `"{0}`" in SublimeText`n{1}" `
+			-f $_.Name,$_.Location
 		subl $_.Location
 	}
 }
